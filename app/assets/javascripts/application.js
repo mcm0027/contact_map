4bbf5ccf6f68c28f9e32e2de73dae5051520db76
Matt Morris <mcm0027@yahoo.com>
2016-01-26 11:56:46 -0600

    link vendor assets in application files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,13 @@
+//= require jquery
 //= require angular
+//= require underscore
 //= require angular-ui-router
-//= require
-//= require
-//= require
-//= require
-//= require
-//= require
-//= require
-//= require
+//= require moment
+//= require moment-timezone
+//= require bootstrap
+//= require chosen
+//= require jstzdetect
+//= require lodash
+//= require angular-timezone-selector
 
 //= require_tree .
